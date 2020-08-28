62bfde183f0d8f60f0bec603e2b974f98bfe5d08
Jolanta Piaseczna <piaseczna@onet.pl>
2020-08-28 10:57:01 +0200

    Add event.preventDefault()

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
 'use strict';
 
 function titleClickHandler(event){
+	event.preventDefault();
 	const clickedElement = this;
   console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+	const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
+	for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
-}
+	}
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
 
-console.log('clickedElement:', clickedElement);	
-clickedElement.classList.add('active');
+	console.log('clickedElement:', clickedElement);	
+	clickedElement.classList.add('active');
 		
 
   /* [DONE] remove class 'active' from all articles */
 
-const activeArticles = document.querySelectorAll('.posts article.active');
+	const activeArticles = document.querySelectorAll('.posts article.active');
 
-for(let activeArticle of activeArticles){
+	for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
-}  
+	}  
 
   /* get 'href' attribute from the clicked link */
 
