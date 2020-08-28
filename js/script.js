6c1ba42d526c625f0e0f51032e6abdbb946eece5
Jolanta Piaseczna <piaseczna@onet.pl>
2020-08-28 13:45:07 +0200

    Add targetArticle.classList.add('active')

--- js/script.js
+++ js/script.js
@@ -15,9 +15,8 @@ function titleClickHandler(event){
 
   /* [DONE] add class 'active' to the clicked link */
 
+  	clickedElement.classList.add('active');
 	console.log('clickedElement:', clickedElement);	
-	clickedElement.classList.add('active');
-		
 
   /* [DONE] remove class 'active' from all articles */
 
@@ -32,12 +31,14 @@ function titleClickHandler(event){
   const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
