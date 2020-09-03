824d7e2e2d6c67f53753e15fe1bd886870cb12aa
Jolanta Piaseczna <piaseczna@onet.pl>
2020-09-04 00:25:24 +0200

    Add tag link to html variable

--- js/script.js
+++ js/script.js
@@ -138,6 +138,9 @@ function generateTags(){
 
       /* add generated code to html variable */
 
+      html = html + linkHTML;
+      console.log(html);
+
     /* END LOOP: for each tag */
     }
 
