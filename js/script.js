a211c7df944181d3a868833a92c616f9c01aec0d
Jolanta Piaseczna <piaseczna@onet.pl>
2020-09-04 00:23:00 +0200

    Generate tag link html

--- js/script.js
+++ js/script.js
@@ -129,8 +129,12 @@ function generateTags(){
     /* START LOOP: for each tag */
 
     for(let tag of articleTagsArray) {
+      console.log(tag);
 
       /* generate HTML of the link */
+
+      const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
+      console.log(linkHTML);
 
       /* add generated code to html variable */
 
